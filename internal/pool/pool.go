d4d680a5083abaca21f28ec61288700bce75d7e4
Jongwoo Han <jongwooo.han@gmail.com>
2025-07-15 01:05:11 +0900

    refactor(pool): Remove DocumentPool implementation

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -4,36 +4,6 @@ import (
 	"sync"
 )
 
-// DocumentPool provides a pool of reusable document maps to reduce memory allocations.
-type DocumentPool struct {
-	pool sync.Pool
-}
-
-// NewDocumentPool creates a new document pool with optimized initial capacity.
-func NewDocumentPool() *DocumentPool {
-	return &DocumentPool{
-		pool: sync.Pool{
-			New: func() interface{} {
-				m := make(map[string]interface{}, 10)
-				return &m
-			},
-		},
-	}
-}
-
-// Get retrieves a document map pointer from the pool or creates a new one.
-func (p *DocumentPool) Get() *map[string]interface{} {
-	return p.pool.Get().(*map[string]interface{})
-}
-
-// Put returns a document map pointer to the pool after clearing its contents.
-func (p *DocumentPool) Put(doc *map[string]interface{}) {
-	for k := range *doc {
-		delete(*doc, k)
-	}
-	p.pool.Put(doc)
-}
-
 // ChunkPool provides a pool of reusable document chunks to reduce slice allocations.
 type ChunkPool struct {
 	pool sync.Pool
